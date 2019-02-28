a75763c24adb069d0ea825f47543681143d80f0d
Shandra <shandradev85@gmail.com>
2019-02-27 18:44:17 -0700

    updated routes

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,13 +1,33 @@
 class SubsController < ApplicationController
   def index
+    @subs = Sub.all
   end
 
   def show
+    @sub = Sub.find(params[:id])
   end
 
   def new
+
   end
 
   def edit
+
   end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+
+  private
+
+
+
+  
 end
